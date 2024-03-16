65f02ea9b816426b6741da5b7d46d0a1c553c1fe
none <none>
2024-03-16 05:36:05 +0100

    Fixed Comments

--- src/AtlasLemmyComment.tsx
+++ src/AtlasLemmyComment.tsx
@@ -20,16 +20,14 @@ function Comment({
   ratioDetector,
   commentDepth = 0,
   showUserAvatar = true,
-  isContext = false,
   isOpen = true,
 }) {
   const [open, setOpen] = useState(isOpen);
   const [replies, setReplies] = useState(null);
-  const [context, setContext] = useState(null);
-
-  let client: LemmyHttp = new LemmyHttp(lemmyInstance?.baseUrl);
 
   function handleReplies() {
+    let client: LemmyHttp = new LemmyHttp(lemmyInstance?.baseUrl);
+
     let form: GetComments = {
       parent_id: post?.comment.id,
       max_depth: 1,
@@ -44,41 +42,6 @@ function Comment({
   useEffect(() => {
     if (post?.comment?.deleted) setOpen(false);
   }, [post?.comment?.deleted]);
-
-  function getContext(comment) {
-    // Split the path string by periods to get an array of IDs
-    const ids = comment.path.split(".");
-
-    // Find the index of the target ID in the array
-    const index = ids.indexOf(comment.id.toString());
-
-    // If the target ID is not found or it's the first ID, return null
-    if (index === -1 || index === 0) {
-      return null;
-    }
-    // Calculate the remaining amount of IDs
-    const contextDepth = ids.length - 1;
-
-    // -1 -1 for 0 and index in ids Array
-    const contextID = ids[index - 1 - 1];
-    // Return the ID before the target ID in the array
-
-    return { contextID, contextDepth };
-  }
-
-  const { contextID, contextDepth } = getContext(post?.comment);
-
-  function handleContext() {
-    let form: GetComments = {
-      parent_id: contextID,
-      max_depth: 1,
-      sort: sort,
-    };
-
-    client.getComments(form).then((res) => {
-      setContext(res?.comments);
-    });
-  }
 
   return (
     <Collapsible.Root
@@ -154,106 +117,58 @@ function Comment({
           {commentDepth < 1 && (post?.post?.nsfw || post?.community?.nsfw) && (
             <p className="post-alert">NSFW</p>
           )}
-          {/* Context */}
 
+          {/* Comment Body */}
+          {post?.comment?.removed && <p className="comment-body">🚮 Comment removed.</p>}
+          {post?.comment?.deleted && <p className="comment-body">🗑️ Comment deleted.</p>}
+          {!(post?.comment?.removed || post?.comment?.deleted) &&
+            post?.comment.content && (
+              <ReactMarkdown className="comment-body">
+                {post?.comment.content}
+              </ReactMarkdown>
+            )}
+
+          {/* Replies */}
           <div
-            className={
-              commentDepth
-                ? `post-reply-container post-reply-depth-${(commentDepth % 7) + 1}`
-                : ``
-            }
+            className={`post-reply-container post-reply-depth-${(commentDepth % 7) + 1}`}
           >
-            {/* Context Count */}
+            {/* Reply Count */}
             {post?.counts.child_count > 0 && !replies && (
               <p
                 className="reply-button"
                 role="button"
                 tabIndex={0}
                 aria-label="Show Replies"
-                onClick={handleContext}
+                onClick={handleReplies}
                 onKeyDown={(e) => {
                   if (e.key === "Enter" || e.key === "Space") {
-                    handleContext;
+                    handleReplies;
                   }
                 }}
               >
                 <span className="post-replycount-icon">💬</span>
-                {`${contextDepth} context${contextDepth > 1 ? "s" : ""}`}
+                {`${post?.counts.child_count} repl${
+                  post?.counts.child_count > 1 ? "ies" : "y"
+                }`}
               </p>
             )}
 
-            {/* Context Comments */}
-            {context && (
-              <Comment
-                community={community}
-                post={context}
-                lemmyInstance={lemmyInstance}
-                commentDepth={commentDepth + 1}
-                sort={sort}
-                ratioDetector={post?.counts.score}
-              />
-            )}
-
-            {/* Comment Body */}
-            {post?.comment?.removed && (
-              <p className="comment-body">🚮 Comment removed.</p>
-            )}
-            {post?.comment?.deleted && (
-              <p className="comment-body">🗑️ Comment deleted.</p>
-            )}
-            {!(post?.comment?.removed || post?.comment?.deleted) &&
-              post?.comment.content && (
-                <ReactMarkdown className="comment-body">
-                  {post?.comment.content}
-                </ReactMarkdown>
-              )}
-            {/* Replies */}
-
-            {isContext && (
-              <div
-                className={`post-reply-container post-reply-depth-${
-                  (commentDepth % 7) + 1
-                }`}
-              >
-                {/* Reply Count */}
-                {post?.counts.child_count > 0 && !replies && (
-                  <p
-                    className="reply-button"
-                    role="button"
-                    tabIndex={0}
-                    aria-label="Show Replies"
-                    onClick={handleReplies}
-                    onKeyDown={(e) => {
-                      if (e.key === "Enter" || e.key === "Space") {
-                        handleReplies;
-                      }
-                    }}
-                  >
-                    <span className="post-replycount-icon">💬</span>
-                    {`${post?.counts.child_count} repl${
-                      post?.counts.child_count > 1 ? "ies" : "y"
-                    }`}
-                  </p>
-                )}
-
-                {/* Reply Comments */}
-                {replies &&
-                  replies.map((reply, index) => {
-                    if (reply.comment.id == post?.comment.id) return; // filter parent commment from API response
-                    return (
-                      <Comment
-                        key={`${reply.creator.id}${reply.comment.id}${index}`}
-                        community={community}
-                        post={reply}
-                        lemmyInstance={lemmyInstance}
-                        commentDepth={commentDepth + 1}
-                        sort={sort}
-                        ratioDetector={post?.counts.score}
-                      />
-                    );
-                  })}
-              </div>
-            )}
+            {/* Reply Comments */}
+            {replies &&
+              replies.map((reply, index) => {
+                if (reply.comment.id == post?.comment.id) return; // filter parent commment from API response
+                return (
+                  <Comment
+                    key={`${reply.creator.id}${reply.comment.id}${index}`}
+                    community={community}
+                    post={reply}
+                    lemmyInstance={lemmyInstance}
+                    commentDepth={commentDepth + 1}
+                    sort={sort}
+                    ratioDetector={post?.counts.score}
+                  />
+                );
+              })}
           </div>
         </>
       </Collapsible.Content>
