998bf3bc25cb88266e4d3fbf126972ec17a8783f
none <none>
2024-12-08 17:22:57 +0100

    Install React-Router

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      "/.netlify/functions/": {
-        target: "http://localhost:8888", // Update with your Netlify Dev server port
+      '/.netlify/functions/': {
+        target: 'http://localhost:8888', // Update with your Netlify Dev server port
         changeOrigin: true,
-        rewrite: (path) => path.replace("/.netlify/functions/", ""),
+        rewrite: (path) => path.replace('/.netlify/functions/', ''),
       },
     },
   },
