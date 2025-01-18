f25bde7510068f6ebc9bee8bb31e9f9dd9cd6420
none <none>
2025-01-18 19:56:36 +0100

    Map Interface

--- src/components/shared/AtlasGeographyOptions.tsx
+++ src/components/shared/AtlasGeographyOptions.tsx
@@ -28,99 +28,8 @@ function AtlasGeographyOptions() {
   } = useContext(AtlasContext)!;
 
   return (
-    <div className="administrative-region">
-      {(activeAdministrativeRegion?.country !== 'country' ||
-        isLocationSelectMode) && (
-        <>
-          {!isLocationSelectMode && (
-            <div className="administrative-region-flag-container">
-              <img
-                className="administrative-region-flag"
-                src={activeAdministrativeRegion?.image}
-                alt={`Flag of ${activeAdministrativeRegion?.country}`}
-              />
-            </div>
-          )}
-          <div className="administrative-region-container">
-            <h1
-              className={`location-name ${
-                activeGeographicIdentifier === 'name' &&
-                'active-geographic-identifier'
-              }`}
-              role="button"
-              aria-label={`Select ${activeAdministrativeRegion?.name}`}
-              tabIndex={0}
-              onClick={() =>
-                setActiveGeographicIdentifier(geographicIdentifiers[0])
-              }
-              onKeyDown={(e) => {
-                if (e.key === 'Enter' || e.key === 'Space') {
-                  setActiveGeographicIdentifier(geographicIdentifiers[1]);
-                }
-              }}
-            >
-              {activeAdministrativeRegion?.name}
-            </h1>
-            <h5
-              className={`country-name ${
-                activeGeographicIdentifier === 'country' &&
-                'active-geographic-identifier'
-              }`}
-              role="button"
-              aria-label={`Select ${activeAdministrativeRegion?.country}`}
-              tabIndex={0}
-              onClick={() =>
-                setActiveGeographicIdentifier(geographicIdentifiers[1])
-              }
-              onKeyDown={(e) => {
-                if (e.key === 'Enter' || e.key === 'Space') {
-                  setActiveGeographicIdentifier(geographicIdentifiers[0]);
-                }
-              }}
-            >
-              {activeAdministrativeRegion?.country}
-            </h5>
-            {geographicIdentifiers.map((type, index) => {
-              if (activeAdministrativeRegion[type] === '') return;
-              if (type === 'name') return;
-              if (type === 'country') return;
-              if (type === 'image') return;
-              if (type === 'id') return;
-              if (type === 'country-code') return;
-              if (type === 'region-code') return;
-              if (type === 'sub-region-code') return;
-              if (type === 'intermediate-region-code') return;
-              if (type === 'emoji') return;
-              if (type === 'ISO3166-2') return;
-              if (type === 'ISO3166-3') return;
-
-              return (
-                <p
-                  key={index}
-                  className={`country-name country-${type} ${
-                    activeGeographicIdentifier === type &&
-                    'active-geographic-identifier'
-                  }`}
-                  role="button"
-                  tabIndex={0}
-                  aria-label={`Select ${activeAdministrativeRegion[type]}`}
-                  onClick={() => setActiveGeographicIdentifier(type)}
-                  onKeyDown={(e) => {
-                    if (e.key === 'Enter' || e.key === 'Space') {
-                      setActiveGeographicIdentifier(
-                        geographicIdentifiers[type],
-                      );
-                    }
-                  }}
-                >
-                  {activeAdministrativeRegion[type]}
-                </p>
-              );
-            })}
-          </div>
-        </>
-      )}
-
+    <>
+      {' '}
       {/* CLICK HISTORY */}
       {administrativeRegionClickHistoryArray.length > 2 && (
         <Collapsible.Root
@@ -179,7 +88,100 @@ function AtlasGeographyOptions() {
           </Collapsible.Content>
         </Collapsible.Root>
       )}
-    </div>
+      <div className="administrative-region">
+        {(activeAdministrativeRegion?.country !== 'country' ||
+          isLocationSelectMode) && (
+          <>
+            {!isLocationSelectMode && (
+              <div className="administrative-region-flag-container">
+                <img
+                  className="administrative-region-flag"
+                  src={activeAdministrativeRegion?.image}
+                  alt={`Flag of ${activeAdministrativeRegion?.country}`}
+                />
+              </div>
+            )}
+            <div className="administrative-region-container">
+              <h1
+                className={`location-name ${
+                  activeGeographicIdentifier === 'name' &&
+                  'active-geographic-identifier'
+                }`}
+                role="button"
+                aria-label={`Select ${activeAdministrativeRegion?.name}`}
+                tabIndex={0}
+                onClick={() =>
+                  setActiveGeographicIdentifier(geographicIdentifiers[0])
+                }
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter' || e.key === 'Space') {
+                    setActiveGeographicIdentifier(geographicIdentifiers[1]);
+                  }
+                }}
+              >
+                {activeAdministrativeRegion?.name}
+              </h1>
+              <h5
+                className={`country-name ${
+                  activeGeographicIdentifier === 'country' &&
+                  'active-geographic-identifier'
+                }`}
+                role="button"
+                aria-label={`Select ${activeAdministrativeRegion?.country}`}
+                tabIndex={0}
+                onClick={() =>
+                  setActiveGeographicIdentifier(geographicIdentifiers[1])
+                }
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter' || e.key === 'Space') {
+                    setActiveGeographicIdentifier(geographicIdentifiers[0]);
+                  }
+                }}
+              >
+                {activeAdministrativeRegion?.country}
+              </h5>
+              {geographicIdentifiers.map((type, index) => {
+                if (activeAdministrativeRegion[type] === '') return;
+                if (type === 'name') return;
+                if (type === 'country') return;
+                if (type === 'image') return;
+                if (type === 'id') return;
+                if (type === 'country-code') return;
+                if (type === 'region-code') return;
+                if (type === 'sub-region-code') return;
+                if (type === 'intermediate-region-code') return;
+                if (type === 'emoji') return;
+                if (type === 'ISO3166-2') return;
+                if (type === 'ISO3166-3') return;
+
+                return (
+                  <p
+                    key={index}
+                    className={`country-name country-${type} ${
+                      activeGeographicIdentifier === type &&
+                      'active-geographic-identifier'
+                    }`}
+                    role="button"
+                    tabIndex={0}
+                    aria-label={`Select ${activeAdministrativeRegion[type]}`}
+                    onClick={() => setActiveGeographicIdentifier(type)}
+                    onKeyDown={(e) => {
+                      if (e.key === 'Enter' || e.key === 'Space') {
+                        setActiveGeographicIdentifier(
+                          geographicIdentifiers[type],
+                        );
+                      }
+                    }}
+                  >
+                    {activeAdministrativeRegion[type]}
+                  </p>
+                );
+              })}
+            </div>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
